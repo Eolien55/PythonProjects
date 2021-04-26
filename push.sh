52cb6bc60b9bec23ac17ae9b97e427e849b3fca1
Elie <elielevaillant2007@gmail.com>
2021-04-26 08:45:12 +0200

    "Changed push.sh for use ssh and add commit messages"

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git init
 git add .
 echo "Commit message : "
 read commit
-git commit -a -m $commit
+git commit -a -m "$commit"
 git push --set-upstream git@github.com:Eolien55/PythonProjects.git master
