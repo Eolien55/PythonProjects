e5c3c70f7cd29db27582d05f0320b6768c6611d0
Eolien55 <elielevaillant2007@gmail.com>
2021-04-28 12:43:48 +0200

    Test

--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@ echo "Commit message : "
 read commit
 echo "Which branch ? [branch]"
 read branch
+git checkout $branch
 git add .
 git commit -a -m "$commit"
 git push --set-upstream git@github.com:Eolien55/PythonProjects.git $branch
