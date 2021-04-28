4df0ab38b18d0c3b8c306528587b4e365a50d1a6
Eolien55 <elielevaillant2007@gmail.com>
2021-04-28 12:30:26 +0200

    Test

--- push.sh
+++ push.sh
@@ -1,14 +1,8 @@
 git init
-git add .
 echo "Commit message : "
 read commit
-echo "\nSpecial branch ? [branch/n]"
+echo "Which branch ? [branch]"
 read branch
-if [ $branch != "n" ] 
-then
-	git checkout $branch
-else 
-	set branch="master"
-fi
+git add .
 git commit -a -m "$commit"
 git push --set-upstream git@github.com:Eolien55/PythonProjects.git $branch
