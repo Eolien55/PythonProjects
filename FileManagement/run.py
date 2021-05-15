77e5168f1337e7ed8e0cb91f8529fda16cc4a375
Eolien55 <elielevaillant2007@gmail.com>
2021-05-15 14:57:12 +0200

    --quiet

--- FileManagement/run.py
+++ FileManagement/run.py
@@ -3,6 +3,7 @@ import sys
 import time
 from main import *
 
+os.chdir("/home/elie/Documents/Scolaire")
 run = ManageFileType()
 val = 0
 for matiere in run.matiere:
@@ -17,10 +18,11 @@ for matiere in run.matiere:
             )
         )
 while True:
+    os.system("git add .; git commit -m 'Save' --quiet; git push --quiet")
     run.check()
     run.check(r"/home/elie/Documents/usb")
     # os.system("black /home/elie/pythonprojects")
     if not val % 600:
         os.system("""notify-send "Vos fichiers ont bien \u00e9t\u00e9 rang\u00e9s" """)
     val += 1
-    time.sleep(0.5)
+    time.sleep(5)
