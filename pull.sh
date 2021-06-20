45cc1ffcebe26cb08550e7267c04bda813d651e5
Eolien55 <elielevaillant2007@gmail.com>
2021-06-20 14:59:02 +0200

    removed useless files

--- pull.sh
+++
@@ -1,6 +0,0 @@
-git init
-echo "Commit message : "
-read commit
-git add .
-git commit -a -m "$($commit)"
-git pull git@github.com:Eolien55/PythonProjects.git
--- push.sh
+++
@@ -1,9 +0,0 @@
-git init
-echo "Commit message : "
-read commit
-echo "Which branch ? [branch]"
-read branch
-git checkout $branch
-git add .
-git commit -a -m "$commit"
-git push --set-upstream git@github.com:Eolien55/PythonProjects.git $branch
